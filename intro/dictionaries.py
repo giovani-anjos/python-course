08dbf40ed01200f499ba3a7f4777dc5947154b89
Giovani Camargo dos Anjos <giovanicamargodosanjos@gmail.com>
2021-07-01 03:28:13 +0000

    dictonaries file added

---
+++ intro/dictionaries.py
@@ -0,0 +1,8 @@
+my_tuple_users = (
+  {"name":"Giovani", "age":25},
+  {"name":"Gabriel", "age":19},
+  {"name":"Bruna", "age":17}
+)
+
+for user in my_tuple_users:
+  print(f'Name: {user["name"]}, Age: {user["age"]}')
